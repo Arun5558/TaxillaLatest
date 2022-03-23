c75ffdeae00eb50a4f9efe7e725438d00eb3806b
arnandigam <arnandigam@AIPLLTH323>
2022-03-23 16:26:18 +0530

    Added VerifyLoginMethod

--- src/test/java/com/testcases/VerifyLogin.java
+++ src/test/java/com/testcases/VerifyLogin.java
@@ -1,7 +1,14 @@
 package com.testcases;
 
+import java.util.Hashtable;
+
+import org.testng.annotations.Test;
+
 public class VerifyLogin {
 
+	@Test(priority=1,dataProvider="InboundwithoutPartition")
+	public void VerifyLogin(Hashtable<String, String> data)
+	{
 
-
+}
 }
